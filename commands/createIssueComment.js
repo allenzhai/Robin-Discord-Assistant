6e5386fbd2981accf9edd8639d99f959e0826484
Allen Zhai <allenzhai730@gmail.com>
2021-05-17 09:07:04 -0400

    implemented createIssueComment

--- commands/createIssueComment.js
+++ commands/createIssueComment.js
@@ -1,3 +1,26 @@
-module.exports = function(message, args) {
-    message.channel.send("Command not yet built");
+const axios = require('axios');
+
+const API = 'https://robinrestapi.herokuapp.com/';
+
+module.exports = async function(args, repo, owner) {
+    const issueNum = args.shift();
+    const comment = args.join(' ');
+
+    console.log(issueNum, comment);
+
+    var message = `There was an error with updating this issue`;
+
+    var body = {
+        body: comment
+    }
+
+    const result = await axios.post(`${API}issue/${owner}/${repo}/${issueNum}/comment`,
+        body            
+    );
+
+    if (result.status == 200) {
+        message = `Issue number ${issueNum} was successfully updated`;
+    }
+
+    return message;
 }
