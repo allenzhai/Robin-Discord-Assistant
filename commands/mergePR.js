debd29e87d9c249eae613bf474f6725ee6e65991
Allen Zhai <allenzhai730@gmail.com>
2021-06-04 11:01:02 -0400

    updated help and implemtned mergepr

--- commands/mergePR.js
+++ commands/mergePR.js
@@ -1,3 +1,26 @@
+const axios = require('axios');
+
+const API = 'https://robinrestapi.herokuapp.com/';
+
 module.exports = function(message, args) {
-    message.channel.send("Command not yet built");
+    var num = args[0];
+    var merge_message = args[1];
+    var result =  await axios.get(`${API}pr/sha/${owner}/${repo}/${num}`);
+
+
+    var sha = result.data.head.sha;
+    result = await api.mergePR(user, repo, num, sha, merge_message);
+    result =  await axios.get(`${API}pr/${owner}/${repo}/merge/${num}`);
+
+
+    var message = `There was an error with this merge.`;
+    if (result.status == 200) {
+        message = `Merge of pull request number ${num} was successful`;
+    }
+    else if (result.response.status == 405) {
+        message = `Pull request number ${num} is not mergeable. Please
+            resolve conflicts, make sure the p r exists, and then try again`;
+    } 
+
+    return message
 }
